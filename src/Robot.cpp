910bcc24d735d5baa841c81b98c50ce4d78d28c9
kevin <kjm16216@yahoo.com>
2016-07-20 13:38:35 -0400

    Second Draft

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -16,6 +16,10 @@ const int Back_ID		=	7;
 const int XboxL3_ID		=	9;
 const int MOHStartID	=	10;
 const int MOHL3_ID		= 	11;
+const int LTrigger		= 	2;
+const int RTrigger		=	3;
+const double Thresh		=	0.1;
+const double Sensitivity	= .5;
 
 class Robot : public SampleRobot{
 	Joystick OpController;
@@ -50,20 +54,21 @@ public:
 	}
 
 	void OperatorControl(){
+		double Left;
+		double Right;
 		while(IsOperatorControl() && IsEnabled()){
-			double LeftDriveDesired;
-			double RightDriveDesired;
 
 			double Slew     	= 	OpController.GetRawAxis(0);
-			double Throttle 	= 	OpController.GetRawAxis(1);
-			double Twist    	= 	OpController.GetRawAxis(2);
-			bool 	Brake		=	OpController.GetRawButton(RBumper_ID);;
+			double Throttle 	= 	OpController.GetRawAxis(RTrigger);
+//			double Twist    	= 	OpController.GetRawAxis(2);
+			double 	Reverse 	=	OpController.GetRawAxis(LTrigger);
+			bool 	Brake		=	OpController.GetRawButton(RBumper_ID);
 
 	// -----Begin block of drive code
 		if (Brake){		//brake, Bracket level 1
 			Left = 0;
 			Right = 0;
-			} else  (Slew > Thresh){	//If Slew is positive (Thumbstick pushed right), go Right, new bracket L2
+			} else if (Slew > Thresh){	//If Slew is positive (Thumbstick pushed right), go Right, new bracket L2
 					Left = (Throttle - Reverse) * Sensitivity;			//Send Left full power
 					Right = (Throttle - Reverse) * Sensitivity * (1 - Slew);	//Send Right partial power, tempered by how hard the thumbstick is being pushed
 					} else if (Slew < (-1 * Thresh)){	//If Slew is negative (Thumbstick pushed left), go Left, end bracket L2, new bracket L2 ***020516 KJM - added an else here.  May be unnecessary
@@ -74,14 +79,14 @@ public:
 									Left = (Throttle - Reverse) * Sensitivity;
 									Right = (Throttle - Reverse) * Sensitivity;
 								}//end bracket L2
-			} 			}//end bracket L1
+			//} 			}//end bracket L1
 	//------End block of spin in place code
 
 
-			SetMotors(LeftDriveDesired, RightDriveDesired);
+			SetMotors(Left, Right);
 
 
-
+/*
 			if(OpController.GetRawButton(5)){
 				LeftFeedWheel.Set(FEED_WHEEL_IN);
 			}else{
@@ -94,6 +99,7 @@ public:
 				RightFeedWheel.Set(OpController.GetRawAxis(3) * FEED_WHEEL_OUT);
 			}
 
+*/
 
 			Wait(1 / MOTOR_FPS);
 		}
