99a74c9666bca23377d3a720c983126f2e75a66c
kevin <kjm16216@yahoo.com>
2016-07-20 18:52:24 -0400

    New branch for Vlad to work in

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -65,22 +65,8 @@ public:
 			bool 	Brake		=	OpController.GetRawButton(RBumper_ID);
 
 	// -----Begin block of drive code
-		if (Brake){		//brake, Bracket level 1
-			Left = 0;
-			Right = 0;
-			} else if (Slew > Thresh){	//If Slew is positive (Thumbstick pushed right), go Right, new bracket L2
-					Left = (Throttle - Reverse) * Sensitivity;			//Send Left full power
-					Right = (Throttle - Reverse) * Sensitivity * (1 - Slew);	//Send Right partial power, tempered by how hard the thumbstick is being pushed
-					} else if (Slew < (-1 * Thresh)){	//If Slew is negative (Thumbstick pushed left), go Left, end bracket L2, new bracket L2 ***020516 KJM - added an else here.  May be unnecessary
-						Left = (Throttle - Reverse) * Sensitivity * (1 + Slew);		//Send Left partial power, tempered by how hard thumbstick is being pushed left
-						Right = (Throttle - Reverse) * Sensitivity; 			//Send Right full power
-							} else //if (Slew < Thresh && Slew > (-1 * Thresh))
-								{
-									Left = (Throttle - Reverse) * Sensitivity;
-									Right = (Throttle - Reverse) * Sensitivity;
-								}//end bracket L2
-			//} 			}//end bracket L1
-	//------End block of spin in place code
+
+	//------End block of drive code
 
 
 			SetMotors(Left, Right);
